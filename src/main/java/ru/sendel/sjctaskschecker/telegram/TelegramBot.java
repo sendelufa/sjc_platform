0f115ff1e62c71c4d1ec7136518b73a8b599f97a
Konstantin Shibkov <shibkov.k@gmail.com>
2021-08-14 11:40:19 +0500

    add bot.channel param to tests, update exist taskboard

--- src/main/java/ru/sendel/sjctaskschecker/telegram/TelegramBot.java
+++ src/main/java/ru/sendel/sjctaskschecker/telegram/TelegramBot.java
@@ -2,13 +2,17 @@ package ru.sendel.sjctaskschecker.telegram;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.sendel.sjctaskschecker.service.SolutionService;
@@ -30,6 +34,8 @@ public class TelegramBot extends TelegramLongPollingBot {
     private final SolutionService solutionService;
 
     private final Dashboard dashboard;
+
+    Map<String, Integer> taskToMessageMap = new ConcurrentHashMap<>();
 
     public TelegramBot(TelegramMessageService messageService,
         TaskService taskService,
@@ -91,16 +97,45 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    public void sendMessageToChannel(String channelName, String text) {
+    public Message sendMessageToChannel(String channelName, String text) {
         text = text.replaceAll("_", "\\\\_");
         SendMessage sendMessage = new SendMessage().setChatId(channelName).setText(text.trim());
         sendMessage = sendMessage.enableMarkdown(true).disableWebPagePreview();
         try {
-            execute(sendMessage);
+            Message m = execute(sendMessage);
             log.info("Message with board to channel {} sent", channelName);
+            return m;
         } catch (TelegramApiException e) {
             log.error("error while send msg to tg_channel " + channelName, e);
         }
+        throw new RuntimeException("strange error while send message");
+    }
+
+    public void sendMessageToChannel(String channelName, String text, String taskId) {
+        Integer messageId = taskToMessageMap.get(taskId);
+        if (messageId == null) {
+            Message message = sendMessageToChannel(channelName, text);
+            taskToMessageMap.put(taskId, message.getMessageId());
+            log.info("new board message in channel, message id:{}", message.getMessageId());
+        } else {
+            updateMessageInChannel(channelName, text, messageId);
+            log.info("update board message in channel, message id:{}", messageId);
+        }
+    }
+
+    private void updateMessageInChannel(String channelName, String text, Integer messageId) {
+        text = text.replaceAll("_", "\\\\_");
+        EditMessageText editMessage = new EditMessageText().setMessageId(messageId)
+            .setChatId(channelName).setText(text.trim());
+        editMessage = editMessage.enableMarkdown(true).disableWebPagePreview();
+        try {
+            execute(editMessage);
+            log.info("MessageID{} with board to channel {} UPDATED", messageId, channelName);
+        } catch (TelegramApiException e) {
+            log.error("error while update msg_id=" + messageId + " to tg_channel " + channelName,
+                e);
+        }
+        throw new RuntimeException("strange error while update message");
     }
 
     private static List<String> separate(String text, int maxLengthMessage) {
